15a4af8077bf732b591bfa81729fef3c4cb9c23a
Narayan Sambi Reddy <narayan-sambireddy@outlook.com>
2018-07-10 08:15:36 +0530

    Added demo module

---
+++ narayan-components-swagger-demo/src/main/java/narayan/components/swagger/demo/NarayanComponentsSwaggerDemoApplication.java
@@ -0,0 +1,12 @@
+package narayan.components.swagger.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NarayanComponentsSwaggerDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NarayanComponentsSwaggerDemoApplication.class, args);
+	}
+}
---
+++ narayan-components-swagger-demo/src/test/java/narayan/components/swagger/demo/NarayanComponentsSwaggerDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package narayan.components.swagger.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NarayanComponentsSwaggerDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
